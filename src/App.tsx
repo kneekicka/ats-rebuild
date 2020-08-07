7b2376f4e4888bc18a65a3aeccd9fe7c052030b9
Mykyta Simogin <mykita.simogin@evia.de>
2020-08-07 14:19:47 +0200

    App fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Search from "./Containers/Search/Search";
 import NavBar from "./Components/Navbar/Navbar";
